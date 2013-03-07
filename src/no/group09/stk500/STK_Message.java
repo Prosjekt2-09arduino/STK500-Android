bed69d7a37c253527c67acbca4808db081c3187d
NinaMargrethe <smorsgar@stud.ntnu.no>
2013-03-07 15:08:29 +0100

    Created enum STK_Message.

---
+++ src/no/group09/stk500/STK_Message.java
@@ -0,0 +1,11 @@
+package no.group09.stk500;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: pinkster
+ * Date: 3/7/13
+ * Time: 3:02 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class STK_Message {
+}
