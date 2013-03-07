71f2e83d2bfcd410d789e7ce6ed71b24fa9b9f63
Jeppe Eriksen <jeppeer@gmail.com>
2013-03-07 14:15:59 +0100

    create src folder

---
+++ src/no/group09/stk500/Constants.java
@@ -0,0 +1,15 @@
+package no.group09.stk500;
+
+/**
+ * Class holding all the constants for the STK500 protocol.
+ * 
+ * @author JeppeE
+ *
+ */
+public class Constants {
+	public static final byte MESSAGE_START = (byte) 0x1B;
+	public static final byte TOKEN = (byte) 0x0E;
+	public static final byte CMD_SIGN_ON = (byte) 0x01;
+	public static final byte STATUS_CMD_OK = (byte) 0x00;
+	
+}
