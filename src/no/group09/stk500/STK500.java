3262ebc3360e75e030115ce80645d289c85d48c6
Jeppe Eriksen <jeppeer@gmail.com>
2013-03-08 15:38:40 +0100

    Work in sceleton for STK500

--- src/no/group09/stk500/STK500.java
+++ src/no/group09/stk500/STK500.java
@@ -1,5 +1,23 @@
 package no.group09.stk500;
 
-public abstract class STK500 {
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+
+public class STK500 {
 	
+	private BufferedOutputStream output;
+	private BufferedInputStream input;
+	
+	public STK500 (BufferedOutputStream output, BufferedInputStream input) {
+		this.output = output;
+		this.input = input;
+	}
+	
+	public void send(byte[] data) {
+		
+	}
+	
+	private byte[] read() {
+		return null;
+	}
 }
