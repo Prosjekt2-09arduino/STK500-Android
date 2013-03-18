e22ef1e29542330aad842ecf5c8d91de82c0157b
bjorfoss <bjofoss2@hotmail.com>
2013-03-18 18:57:44 +0100

    Changes related to sending and receiving
    Most changes related to #10
     * Implement sending
     * Implement receiving
     * Add method to check programmer version on the Arduino
     * Handle sequence numbers #9

--- src/no/group09/stk500/STK500.java
+++ src/no/group09/stk500/STK500.java
@@ -2,10 +2,14 @@ package no.group09.stk500;
 
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
 
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 public class STK500 {
+	/**Size of message field without the message body**/
+	public static final int MESSAGE_HEADER_SIZE = 6;
 	
 	private BufferedOutputStream output;
 	private BufferedInputStream input;
@@ -20,14 +24,43 @@ public class STK500 {
 		this.output = output;
 		this.input = input;
 		sequenceNumber = 0;
-		throw new NotImplementedException();
+		System.out.println(getProgrammerVersion());
 	}
 	
-	private void send(byte[] data) {
-		throw new NotImplementedException();
+	/**
+	 * Discover if you're dealing with a STK500 or AVRISP
+	 * @return
+	 */
+	private String getProgrammerVersion() {
+		//send command to get AVRISP or STK500 response
+		byte[] signBody = {STK_Message.CMD_SIGN_ON.getByteValue()}; 
+		
+		try {
+			send(signBody);
+			Message version = read();
+
+			byte[] body;
+			String ret = "";
+			body = version.getBody();
+			if (body[0] != STK_Message.CMD_SIGN_ON.getByteValue() {
+				throw new RuntimeException("Wrong response ID: expected " +
+						STK_Message.CMD_SIGN_ON.getByteValue() + ", but " +
+						"received " + body[0] + ".");
+			}
+			//convert byte array to string
+			for (int i = 0; i < body.length; i++) {
+				byte b = body[i];
+				ret += (char) decodeByte(b);
+			}
+		} catch (IOException e) {
+			// TODO Retry x times
+			e.printStackTrace();
+		}
 	}
 	
-	
+	private void EnterProgrammingMode() {
+		throw new NotImplementedException();
+	}
 	
 	/**
 	 * @return Sequence number between 0 and 255; wraps back to 0.
@@ -39,8 +72,47 @@ public class STK500 {
 	}
 	
 	
-	private byte[] read() {
-		throw new NotImplementedException();
+	/**
+	 * Create a message and send it to the Arduino
+	 * @param body Command to send
+	 * @throws IOException
+	 */
+	private void send(byte[] body) throws IOException {
+		Message message = new Message(getNewSequenceNumber(), body);
+		byte[] bytes = message.getData();
+		output.write(bytes, 0, bytes.length);
+	}
+
+	/**
+	 * Reads the response from the Arduino, sends it to the Message class
+	 * for parsing and returns the completed message.
+	 * @return Message with response
+	 * @throws IOException
+	 */
+	private Message read() throws IOException {
+		Message response;
+		//The read byte as an int or -1 for nothing more to read
+		int read = 0;
+		ArrayList<Byte> data = new ArrayList<Byte>();
+		while(read != -1) {
+			read = input.read();
+			if (read != -1) {
+				data.add((byte) read);
+			} else {
+				//stop reading - create message and change sequence number
+				response = new Message(data);
+				int responseSequenceNumber = response.getSequenceNumber();
+				if (responseSequenceNumber == sequenceNumber + 1 ||
+						(responseSequenceNumber == 0 && sequenceNumber == 255)) {
+				} else {
+					//sequence number doesn't match
+					throw new IllegalArgumentException("Wrong sequence number:" +
+							"old was " + sequenceNumber + " and the received "+ 
+							"message is " + responseSequenceNumber);
+				}
+				return response;
+			}
+		}
 	}
 	
 	/**
