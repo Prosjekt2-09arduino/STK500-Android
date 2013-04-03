7a1df2239fbc3425deb8b32f7b81733adf9083a8
Bjørn Arve Fossum <bjofoss2@hotmail.com>
2013-04-03 16:51:45 +0200

    Protocol handshake #6
    * Implement checkIfStarterKitPresent()
    * Achieve handshake

--- src/no/group09/stk500_v1/STK500v1.java
+++ src/no/group09/stk500_v1/STK500v1.java
@@ -1,7 +1,9 @@
 package no.group09.stk500_v1;
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.Arrays;
 
 
 public class STK500v1 {
@@ -16,10 +18,74 @@ public class STK500v1 {
 		this.logger = log;
 		this.binary = binary;
 		log.debugTag("Initializing programmer");
+		
+		//try to get programmer version
+		String version = checkIfStarterKitPresent();
+		log.debugTag(version);
+		log.printToConsole(version);
 	}
 	
 	
-	private void checkIfStarterKitPrestent() {
+	private String checkIfStarterKitPresent() {
+		logger.debugTag("Detect programmer");
+		String version = "";
+		
+		//Send request
+		try {
+			byte[] out = new byte[] {
+					ConstantsStk500v1.STK_GET_SIGN_ON, ConstantsStk500v1.CRC_EOP
+			};
+			output.write(out);
+			logger.debugTag("Sending bytes: " + Arrays.toString(out));
+		} catch (IOException e) {
+			logger.debugTag("Communication problem: Can't send request for programmer version");
+			return "-1";
+		}
+		
+		//Read response
+		try {
+			char[] response = new char[7];
+			int responseIndex = 0;
+			int readResult = 0;
+			byte readByte;
+			while (readResult >= 0) {
+				readResult = input.read();
+				if (readResult == -1) {
+					//TODO: Discover when/if this happens
+					logger.debugTag("End of stream encountered in checkIfStarterKitPresent()");
+					break;
+				}
+				readByte = (byte) readResult;
+				logger.debugTag("Read byte: " + readByte);
+				if (responseIndex == 0 && readByte == ConstantsStk500v1.STK_INSYNC) {
+					//good response, next byte should be first part of the string
+					//optiboot never sends the string, fix
+					responseIndex = 7;
+					continue;
+				} else if (responseIndex == 7 && readByte == ConstantsStk500v1.STK_OK) {
+					//index too high for array writing, return string if all OK
+					version = "Arduino (HAX)";
+					//version = String.copyValueOf(response);
+					return version;
+				} else if (responseIndex >= 0 && responseIndex < 7) {
+					//read string
+					response[responseIndex] = (char) readByte;
+					responseIndex++;
+				} else if (responseIndex == 0 && readByte == ConstantsStk500v1.STK_NOSYNC){
+					//not in sync
+					//TODO: Consider attempting to get synch
+					logger.debugTag("Unable to synchronize");
+					break;
+				} else {
+					logger.debugTag("Not terminated by STK_OK in checkIfStarterKitPresent()!");
+					break;
+				}
+			}
+		} catch (IOException e) {
+			logger.debugTag("Communication problem: Can't receive programmer version");
+		}
+		
+		return version;
 	}
 	
 	private void getSynchronization() {
