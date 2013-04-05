def6a2e67c4249f76b940cf98f3cbe7fb8e726d4
LaStrada <stale.hauknes@gmail.com>
2013-04-05 11:47:19 +0200

    Add documentation and fix some bugs
    #22

--- src/no/group09/stk500_v1/Hex.java
+++ src/no/group09/stk500_v1/Hex.java
@@ -15,47 +15,71 @@ public class Hex {
 		// count lines and create an array
 		state = splitHex(bin);
 		
-		logger.debugTag("State: " + state);
+//		logger.debugTag("State: " + state);
 	}
 	
+	/**
+	 * Get a line from hex
+	 * @param line
+	 * @return array with size, address (high), address (low) and data
+	 */
 	public byte[] getHexLine(int line)
 	{
 		return formatHexLine(line);
 	}
 	
+	/**
+	 * 
+	 * @return 
+	 */
 	public int getLines()
 	{
 		return line;
 	}
 	
+	/**
+	 * 
+	 * @param l Line of hex
+	 * @return hex file status
+	 */
 	public boolean getChecksumStatus(int l)
 	{
+//		return checkData(l);
 		return true;
 	}	
 	
+	/**
+	 * Split the hex input into an array.
+	 * Size, address (high), address (low), data
+	 * @param subHex
+	 * @return true if the hex file is correct.
+	 */
 	private boolean splitHex(byte[] subHex) {
 		byte[] tempHex = new byte[maxBytesOnLine];
 		
 //		If no bytes left
 		if(subHex.length == 0) {
-			logger.debugTag("ERROR: No bytes left.");
+//			logger.debugTag("ERROR: No bytes left.");
+			System.out.println("ERROR: No bytes left.");
 			return false;
 		}
 //		The line must start with ':'
 		else if(subHex[0] != 58) {
-			logger.debugTag("ERROR: Line not starting with ':' !");
+//			logger.debugTag("ERROR: Line not starting with ':' !");
+			System.out.println("ERROR: Line not starting with ':' !");
 			return false;
 		}
 //		End of hex file
 		else if(subHex[1] == 0 && subHex[2] == 0) {
-			logger.debugTag("End of hex file.");
+//			logger.debugTag("End of hex file.");
+			System.out.println("End of hex file.");
 			return true;
 		}
 		else {
 			tempHex[0] = subHex[1];	// size
 			tempHex[1] = subHex[2];	// start address
 			tempHex[2] = subHex[3];	// end address
-			tempHex[3] = subHex[4];	// record
+//			tempHex[3] = subHex[4];	// record
 			
 //			data
 			int dataLength = tempHex[0];
@@ -64,58 +88,76 @@ public class Hex {
 				tempHex[i] = subHex[i];
 			}
 			
-			tempHex[4+dataLength] = subHex[5+dataLength];	// checksum
+			tempHex[3+dataLength] = subHex[5+dataLength];	// checksum
+			System.out.println("Checksum: " + tempHex[3+dataLength]);
 			
-			for(int i=0; i<dataLength+6; i++) {
-				//binary[line][i] = tempHex[i];
-				binary[line] = Arrays.copyOfRange(subHex, 6+dataLength, subHex.length);
-			}
+//			for(int i=0; i<dataLength+6; i++) {
+//				binary[line][i] = tempHex[i];
+//				binary[line] = Arrays.copyOfRange(subHex, 6+dataLength, subHex.length);
+//			}
+			
+			binary[line] = Arrays.copyOfRange(subHex, 1, dataLength+3);
 			
 			//Send array recursiv
 			line++;
-			return splitHex(Arrays.copyOfRange(subHex, 6+dataLength, subHex.length));
+			return splitHex(Arrays.copyOfRange(subHex, dataLength+6, subHex.length));
 		}
 	}
 	
 	
-//	return size, address x 2, data
+	/**
+	 * Format line in hex file,
+	 * @param line
+	 * @return one line from hex, including size, address (high + low) and data.
+	 */
 	private byte[] formatHexLine(int line)
 	{
-		int length = binary[line][1];
-		
 		byte tempBinary[] = new byte[maxBytesOnLine];
 		
-		tempBinary[0] = binary[line][1];	// Length
-		tempBinary[1] = binary[line][2];	// Address 1
-		tempBinary[2] = binary[line][3];	// Address 2
-		
-//		Data
-		for(int i=4; i<length+4; i++) {
-			tempBinary[i] = binary[line][i];
+		//Check if the line is out of bounds
+		if(line <= this.line) {
+			int length = binary[line][0];
+			
+			tempBinary[0] = binary[line][0];	// Length
+			tempBinary[1] = binary[line][1];	// Address 1
+			tempBinary[2] = binary[line][2];	// Address 2
+			
+			//Data
+			for(int i=3; i<length+2; i++) {
+				tempBinary[i] = binary[line][i];
+			}
 		}
 		
 		return tempBinary;
 	}
 	
-
-	private boolean checkData(int line) {
-		int checksum = 0;
-		
-//		System.out.println("First: " + binary[line][2]);
-		System.out.println(binary[1]);
-		
-		int length = unPackTwoBytes(binary[line][2], binary[line][1]);
-//		System.out.println("Length: " + length);
-		
-		for(int i=0; i<length; i++) {
-			checksum += binary[line][i];
-		}
-		
-//		System.out.println(checksum);
-//		System.out.println(binary[22]);
-		return true;
-	}
-	
+//	/**
+//	 * Check if the checksum is correct.
+//	 * @param line
+//	 * @return true if checksum is correct
+//	 */
+//	private boolean checkData(int line) {
+//		int checksum = 0;
+//		
+//		System.out.println("\nChecksum on line " + line);
+//		
+//		//length of data
+//		int length = binary[line][0];
+//		System.out.println("Length:     " + length);
+//		
+//		//data
+//		for(int i=0; i<length; i++) {
+//			checksum += binary[line][i];
+//		}
+//		
+//		System.out.println("Last byte:  " + binary[line][length+2]);
+//		System.out.println("Sum:        " + checksum);
+//		
+//		System.out.println("Checksum done.\n");
+//		
+//		return true;
+//	}
+//	
 	/**
 	 * Read two unsigned bytes into an integer
 	 * @param high Most significant byte
