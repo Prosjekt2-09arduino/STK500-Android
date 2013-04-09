1179c4b496f0cb86fea16ef776201577a4befd40
LaStrada <stale.hauknes@gmail.com>
2013-04-09 14:52:43 +0200

    Change array to ArrayList #22

--- src/no/group09/stk500_v1/Hex.java
+++ src/no/group09/stk500_v1/Hex.java
@@ -1,11 +1,14 @@
 package no.group09.stk500_v1;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 
 public class Hex {
 	private Logger logger;
 	private final static int maxBytesOnLine = 24;
-	private byte[][] binary = new byte[256][maxBytesOnLine];
+//	private byte[][] binary = new byte[256][maxBytesOnLine];
+	
+	ArrayList<ArrayList<Byte>> binList = new ArrayList<ArrayList<Byte>>();
+	
 	private int line = 0;
 	private boolean state = false;
 	private static final int ADDITION_TO_LENGTH_IN_CHECKSUM = 4;
@@ -20,7 +23,7 @@ public class Hex {
 	}
 	
 	/**
-	 * Get a line from hex
+	 * Return a line from hex.
 	 * @param line
 	 * @return array with size, address (high), address (low) and data
 	 */
@@ -30,18 +33,20 @@ public class Hex {
 	}
 	
 	/**
-	 * 
-	 * @return 
+	 * Returns number of lines in hex file.
+	 * @return line from hex file
 	 */
 	public int getLines()
 	{
+		System.out.println(line);
+		System.out.println(binList.size());
 		return line;
 	}
 	
 	/**
-	 * 
+	 * Return state of hex file.
 	 * @param l Line of hex
-	 * @return hex file status
+	 * @return true if the hex file is correct.
 	 */
 	public boolean getChecksumStatus(int l)
 	{
@@ -50,18 +55,18 @@ public class Hex {
 	}	
 	
 	/**
-	 * Split the hex input into an array.
-	 * Size, address (high), address (low), data
-	 * @param subHex
+	 * Split the hex input into an array and check if it is correct, including the checksum. Each line must start with ':' (colon), byte value 58.
+	 * The following values is 1 byte size, 2 byte address, n byte data, 1 byte checksum.
+	 * @param array with bytes
 	 * @return true if the hex file is correct.
 	 */
 	private boolean splitHex(byte[] subHex) {
-		byte[] tempHex = new byte[maxBytesOnLine-3]; // we do not need start byte, checksum and record
+//		byte[] tempHex = new byte[maxBytesOnLine-3]; // we do not need start byte, checksum and record
 		
 //		If no bytes left
 		if(subHex.length == 0) {
-//			logger.debugTag("ERROR: No bytes left.");
-			System.out.println("ERROR: No bytes left.");
+//			logger.debugTag("ERROR: No bytes left!");
+			System.out.println("ERROR: No bytes left!");
 			return false;
 		}
 //		The line must start with ':'
@@ -72,62 +77,87 @@ public class Hex {
 		}
 //		End of hex file
 		else if(subHex[1] == 0 && subHex[2] == 0) {
-//			logger.debugTag("End of hex file.");
-			System.out.println("End of hex file.");
+//			logger.debugTag("End of hex file!");
+			System.out.println("End of hex file!");
 			return true;
 		}
 		else {
-			//TODO: need to verify checksum here
+			//add new line to ArrayList
+			binList.add(new ArrayList<Byte>());
 			
-			tempHex[0] = subHex[1];	// size
-			tempHex[1] = subHex[2];	// start address
-			tempHex[2] = subHex[3];	// end address
-//			tempHex[3] = subHex[4];	// record
+//			tempHex[0] = subHex[1];	// size
+			binList.get(line).add(subHex[1]); // size
 			
-//			data
-			int dataLength = tempHex[0];
+//			tempHex[1] = subHex[2];	// start address
+			binList.get(line).add(subHex[2]); // start address
 			
-			for (int i = 3; i < dataLength+3; i++) {
-				tempHex[i] = subHex[i+2];
+//			tempHex[2] = subHex[3];	// end address
+			binList.get(line).add(subHex[3]); // start address
+			
+//			tempHex[3] = subHex[4];	// record, only used to calculate checksum
+			binList.get(line).add(subHex[4]); // record
+			
+			//save length
+//			int dataLength = tempHex[0];
+			int dataLength = binList.get(line).get(0);
+
+			//save data
+			for (int i = 5; i < dataLength+5; i++) {
+//				tempHex[i] = subHex[i+2];
+				binList.get(line).add(subHex[i]);
 			}
 			
-			tempHex[3+dataLength] = subHex[5+dataLength];	// checksum
-//			System.out.println("Checksum: " + tempHex[3+dataLength]);
+			//save checksum
+//			tempHex[3+dataLength] = subHex[5+dataLength];
+			binList.get(line).add(subHex[5+dataLength]);
 			
-//			for(int i=0; i<dataLength+6; i++) {
-//				binary[line][i] = tempHex[i];
-//				binary[line] = Arrays.copyOfRange(subHex, 6+dataLength, subHex.length);
-//			}
-			
-			binary[line] = Arrays.copyOfRange(subHex, 1, dataLength+3);
-			
-			//Send array recursiv
-			line++;
-			return splitHex(Arrays.copyOfRange(subHex, dataLength+6, subHex.length));
+			//Check if the checksum is correct
+			if(checkData(line)==true) {
+				//Split the rest of the array
+				byte[] returnHex = new byte[subHex.length-dataLength-6];
+				for (int i = dataLength+6; i < subHex.length; i++) {
+					returnHex[i-dataLength-6] = subHex[i];
+				}
+				
+				line++;
+				return splitHex(returnHex);
+			}
+			else {
+				System.out.println("Error: Checksum failed!");
+				return false;
+			}
 		}
 	}
 	
 	
 	/**
+<<<<<<< HEAD
 	 * Format line in hex file,
 	 * @param l
 	 * @return one line from hex, including size, address (high + low) and data.
+=======
+	 * Format line in hex file into an array with:
+	 * 1 byte size
+	 * 2 byte address
+	 * n byte address
+	 * @param line number in hex file
+	 * @return byte array, empty if the line is out of bounds
+>>>>>>> Change array to ArrayList #22
 	 */
 	private byte[] formatHexLine(int l)
 	{
-		byte tempBinary[] = new byte[maxBytesOnLine];
+		byte tempBinary[] = new byte[maxBytesOnLine-3];
 		
 		//Check if the line is out of bounds
-		if(l <= line) {
-			int length = binary[l][0];
+		if(l < line) {
+			int length = binList.get(l).size();
 			
-			tempBinary[0] = binary[l][0];	// Length
-			tempBinary[1] = binary[l][1];	// Address 1
-			tempBinary[2] = binary[l][2];	// Address 2
-			
-			//Data
-			for(int i=3; i<length+2; i++) {
-				tempBinary[i] = binary[l][i];
+			//Add elements into an array
+			for(int i=0; i<length; i++) {
+				//Ignore the record element
+				if(i!=3) {
+					tempBinary[i] = binList.get(l).get(i);
+				}
 			}
 		}
 		
@@ -147,21 +177,33 @@ public class Hex {
 		System.out.println("Verifying checksum of line: " + line);
 		
 		//length of data
+<<<<<<< HEAD
 		int length = binary[line][0] + ADDITION_TO_LENGTH_IN_CHECKSUM;
+=======
+		int length = binList.get(line).size();
+>>>>>>> Change array to ArrayList #22
 		System.out.println("Number of bytes of data: " + length);
 		
 		int byteValue = 0;
 		
 		//Add the values of all the fields together
 		for(int i=0; i<length-1; i++) {
+<<<<<<< HEAD
 			byteValue += binary[line][i];
+=======
+			byteValue += binList.get(line).get(i);
+>>>>>>> Change array to ArrayList #22
 		}
 		
 		int b = 0x100;
 		
 		byte check = (byte) (b-byteValue);
 		
+<<<<<<< HEAD
 		return (check&0xFF) == (binary[line][length-1]&0xFF);
+=======
+		return (check&0xFF) == (binList.get(line).get(length-1)&0xFF);
+>>>>>>> Change array to ArrayList #22
 	}
 	
 	/**
--- src/no/group09/stk500_v1/HexTest.java
+++ src/no/group09/stk500_v1/HexTest.java
@@ -1,7 +1,5 @@
 package no.group09.stk500_v1;
 
-import java.io.UnsupportedEncodingException;
-
 public class HexTest {
 
 	public static void main(String[] args) {
@@ -11,12 +9,6 @@ public class HexTest {
 				"3A100010000C947E000C947E000C947E000C947E0068" +
 				"3A100010000C947E000C947E000C947E000C947E0068" +
 				"3A00000001FF";
-		
-//		String hexData =
-//				"3A0300300002337A1E";
-		
-//		String hexData =
-//				"3A10246200464C5549442050524F46494C4500464C33";
 		
 		byte[] bytes = new byte[hexData.length() / 2];
 		
@@ -28,26 +20,17 @@ public class HexTest {
 		Logger log = null;
 		
 		Hex hex = new Hex(bytes, log);
-//		boolean state = hex.getChecksumStatus(0);
 		
-//		System.out.println(hex.getLines());
-//		System.out.println(state);
-		
-//		System.out.println("#############");
-//		System.out.println("Line 1:");
-		
-		byte[] testArray = hex.getHexLine(0);
-		System.out.println("#### LINE 0");
-		for (int i = 0; i < testArray.length; i++) {
-			System.out.println(testArray[i]);
+		for(int line=0; line<4; line++) {
+			System.out.println("\n#### LINE: " + line);
+			
+			byte[] testArray = hex.getHexLine(line);
+			for (int i = 0; i < testArray.length; i++) {
+				System.out.println(testArray[i]);
+			}
 		}
 		
-		
-		byte[] testArray1 = hex.getHexLine(1);
-		System.out.println("#### LINE 1");
-		for (int i = 0; i < testArray.length; i++) {
-			System.out.println(testArray1[i]);
-		}
+		System.out.println(hex.getLines());
 	}
 
 }
