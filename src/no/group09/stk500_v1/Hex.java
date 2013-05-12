35ccc718326d9a086d4e09b5d558e28ef3ca0cfb
Wilhelm W Schive <wilumium@hotmail.com>
2013-05-12 13:03:39 +0200

    modified to LESSER

--- src/no/group09/stk500_v1/Hex.java
+++ src/no/group09/stk500_v1/Hex.java
@@ -6,19 +6,18 @@ package no.group09.stk500_v1;
  *  This file is part of STK500ForJava.
  *
  *	STK500ForJava is free software: you can redistribute it and/or modify
- *	it under the terms of the GNU General Public License as published by
+ *	it under the terms of the GNU Lesser General Public License as published by
  *	the Free Software Foundation, either version 3 of the License, or
  *	(at your option) any later version.
  *
  *	STK500ForJava is distributed in the hope that it will be useful,
  *	but WITHOUT ANY WARRANTY; without even the implied warranty of
  *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *	GNU General Public License for more details.
+ *	GNU Lesser General Public License for more details.
  *
- *	You should have received a copy of the GNU General Public License
+ *	You should have received a copy of the GNU Lesser General Public License
  *	along with STK500ForJava.  If not, see <http://www.gnu.org/licenses/>.
  */
-
 import java.util.ArrayList;
 
 /**
