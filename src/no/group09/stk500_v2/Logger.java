bd459b632dfbd548f0f6e2b1272f7c0b9b94bbdc
Wilhelm W Schive <wilumium@hotmail.com>
2013-05-12 12:38:55 +0200

    added licensing for #51

--- src/no/group09/stk500_v2/Logger.java
+++ src/no/group09/stk500_v2/Logger.java
@@ -1,5 +1,24 @@
 package no.group09.stk500_v2;
 
+/**
+ *  Copyright 2013 UbiCollab
+ *  
+ *  This file is part of STK500ForJava.
+ *
+ *	STK500ForJava is free software: you can redistribute it and/or modify
+ *	it under the terms of the GNU General Public License as published by
+ *	the Free Software Foundation, either version 3 of the License, or
+ *	(at your option) any later version.
+ *
+ *	STK500ForJava is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU General Public License for more details.
+ *
+ *	You should have received a copy of the GNU General Public License
+ *	along with STK500ForJava.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 public interface Logger {
 	/**
 	 * Display a message for a brief time
